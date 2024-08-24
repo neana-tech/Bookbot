e1a19bd989e7277d0941c8f838bd608c0e658013
neaena-tech <nukpong1@student.umgc.edu>
2024-08-23 23:33:11 -0400

    Refactor file path to use relative path

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+import os
+
+# Get the directory of the current script
+script_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Change the current working directory to the script directory
+os.chdir(script_dir)
+
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
